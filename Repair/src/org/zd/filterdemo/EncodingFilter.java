d7fa8e056067ab99eda13db793dee33c4f0aa37e
hloffside <179789349@qq.com>
2017-07-15 15:29:14 +0800

    过滤..包括字符集等.....

---
+++ Repair/src/org/zd/filterdemo/EncodingFilter.java
@@ -0,0 +1,27 @@
+package org.zd.filterdemo ;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+public class EncodingFilter implements Filter {
+	private String charSet ;
+	public void init(FilterConfig config)
+          throws ServletException{
+		// 接收初始化的参数
+		this.charSet = config.getInitParameter("charset") ;	
+	}
+	public void doFilter(ServletRequest request,
+              ServletResponse response,
+              FilterChain chain)
+              throws IOException,
+                     ServletException{
+		request.setCharacterEncoding(this.charSet) ;
+		chain.doFilter(request,response) ;
+	}
+	public void destroy(){
+	}
+}
